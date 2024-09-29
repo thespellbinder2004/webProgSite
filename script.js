35964fd05386129af741b55dc09013c7c9ca7425
thespellbinder2004 <josephgabrielcastro20work@gmail.com>
2024-09-29 20:48:31 +0800

    Update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("joseph");
+const btnMenu = document.querySelector("#menu");
+const menuItems = document.querySelector(".menu-items");
+btnMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  menuItems.classList.toggle("hidden");
+});
